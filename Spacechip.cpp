fb263c9a5d1af1006c4e116b24d01918ae8ad019
Adria <adria.cortes@estudiant.enti.cat>
2026-05-29 10:04:08 +0200

    arreglado rotar y mover

--- Spacechip.cpp
+++ Spacechip.cpp
@@ -16,8 +16,8 @@ Spaceship::Spaceship(SDL_Renderer* renderer, Vector2 pos, float rot, Vector2 scl
 	linearDrag = 1.2f;
 	angularDrag = 6.0f;
 
-	linearAccelFactor = 500.0f; // pixels / sec^2
-	angularAccelFactor = 180000.0f; // Deg / sec^2
+	linearAccelFactor = 50.0f; // pixels / sec^2
+	angularAccelFactor = 1800.0f; // Deg / sec^2
 
 	/*linearAcceleration = Vector2(1.5f, 1.5f);
 	angularAcceleration = 1.5f;*/
@@ -25,30 +25,30 @@ Spaceship::Spaceship(SDL_Renderer* renderer, Vector2 pos, float rot, Vector2 scl
 
 
 void Spaceship::UpdateMovement(float dt) {
-	linearAcceleration = Vector2();
+    linearAcceleration = Vector2();
 
-	if (IM.GetKey(SDLK_w, HOLD))
-	{
-		Vector2 dir;
-		float rotInRad = rotation * (M_PI / 180.0f);
-		dir.x = cos(rotation);
-		dir.y = sin(rotation);
+    if (IM.GetKey(SDLK_w, HOLD))
+    {
+        float rotInRad = rotation * (M_PI / 180.0f);
+        Vector2 dir;
+        dir.x = cos(rotInRad);
+        dir.y = sin(rotInRad);
 
-		linearAcceleration = dir * linearAccelFactor;
-	}
-	
-	angularAcceleration = 0.f;
+        linearAcceleration = dir * linearAccelFactor;
+    }
+    
+    angularAcceleration = 0.f;
 
-	if (IM.GetKey(SDLK_d, HOLD))
-	{
-		angularAcceleration = angularAccelFactor * dt;
-	}
+    if (IM.GetKey(SDLK_d, HOLD))
+    {
+        angularAcceleration = angularAccelFactor;
+    }
 
-	if (IM.GetKey(SDLK_a, HOLD))
-	{
-		angularAcceleration = -angularAccelFactor * dt;
-	}
-	
+    if (IM.GetKey(SDLK_a, HOLD))
+    {
+        angularAcceleration = -angularAccelFactor;
+    }
+    
 
-	GameObject::UpdateMovement(dt);
+    GameObject::UpdateMovement(dt);
 }
